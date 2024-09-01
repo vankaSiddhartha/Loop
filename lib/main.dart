8428e03d46b85840bedbbfb567331c884efce9f0
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2024-09-01 17:28:10 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:loop/constants.dart';
+import 'package:loop/controller/auth_controller.dart';
 import 'package:loop/view/screens/auth/sign_in.dart';
 import 'package:loop/view/screens/auth/sign_up.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value){
+   Get.put(AuthController());
+  });
   runApp(const MyApp());
 }
 
@@ -13,7 +20,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Loop',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
